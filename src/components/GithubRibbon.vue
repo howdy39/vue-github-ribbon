7ee1b32733cc2b67b9570597c3129edb5c71ad1b
howdy39 <tatsuya.howdy@gmail.com>
2019-05-19 15:41:58 +0900

    add: props

--- src/components/GithubRibbon.vue
+++ src/components/GithubRibbon.vue
@@ -1,11 +1,90 @@
 <template>
-  <a class="github-fork-ribbon"
-    href="http://url.to-your.repo"
-    title="Fork me on GitHub"
-    >Fork me on GitHub
-  </a>
+  <a
+    :id="componentId"
+    :title="title"
+    :class="[position]"
+    :href="href"
+    class="github-fork-ribbon"
+  >{{title}}</a>
 </template>
 
+<script>
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    href: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      default: "Fork me on GitHub"
+    },
+    position: {
+      type: String,
+      default: "right-top",
+      validator: function(value) {
+        return [
+          "right-top",
+          "right-bottom",
+          "left-top",
+          "left-bottom"
+        ].includes(value);
+      }
+    },
+    color: {
+      type: String,
+      default: "black"
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    componentId() {
+      return `vue-ribbon-${this._uid}`;
+    }
+  },
+  methods: {
+    isLightColor() {
+      let rgb = parseInt(this.color.substring(1), 16);
+      let r = (rgb >> 16) & 0xff;
+      let g = (rgb >> 8) & 0xff;
+      let b = (rgb >> 0) & 0xff;
+      let luma = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+      return luma > 128;
+    },
+    dynamicCSSClass() {
+      let nodeId = "vue-ribbon-bkg-" + this._uid;
+      let style = document.getElementById(nodeId);
+      if (!style) {
+        style = document.createElement("style");
+        style.id = nodeId;
+        style.type = "text/css";
+        this.$el.appendChild(style);
+      }
+      let cssClass = `#${this.componentId}:before { background-color: ${
+        this.color
+      }} `;
+      if (this.isLightColor()) {
+        cssClass += `#${
+          this.componentId
+        }:after { color: #000; border-color: #000;}`;
+      }
+      style.innerHTML = cssClass;
+    }
+  },
+  mounted: function() {
+    this.dynamicCSSClass();
+  },
+  beforeUpdate: function() {
+    this.dynamicCSSClass();
+  }
+});
+</script>
+
+
 <style lang="scss" scoped>
-@import '~github-fork-ribbon-css/gh-fork-ribbon.css';
+@import "~github-fork-ribbon-css/gh-fork-ribbon.css";
 </style>
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -35,10 +35,45 @@ storiesOf('Button', module)
     methods: { action: action('clicked') },
   }));
 
-storiesOf('Ribbon', module).add('with basic', () => ({
-  components: { GithubRibbon },
-  template: '<github-ribbon />',
-  methods: { action: linkTo('Button') },
-}));
-  
+storiesOf('Ribbon', module)
+  .add('basic', () => ({
+    components: { GithubRibbon },
+    template: `
+  <div>
+    <github-ribbon href="https://github.com/howdy39" />
+  </div> 
+  `
+  }))
+  .add('title', () => ({
+    components: { GithubRibbon },
+    template: `
+  <div>
+    <github-ribbon href="https://github.com/howdy39" title="howdy!" />
+  </div>
+  `
+  }))
+  .add('position', () => ({
+    components: { GithubRibbon },
+    template: `
+  <div>
+    <github-ribbon href="https://github.com/howdy39" position="right-top" title="right-top" />
+    <github-ribbon href="https://github.com/howdy39" position="left-top" title="left-top" />
+    <github-ribbon href="https://github.com/howdy39" position="left-bottom" title="left-bottom" />
+    <github-ribbon href="https://github.com/howdy39" position="right-bottom" title="right-bottom" />
+  </div>
+  `
+  }))
+  .add('color', () => ({
+    components: { GithubRibbon },
+    template: `
+  <div>
+    <github-ribbon href="https://github.com/howdy39" position="right-top" title="#000000" color="#000000"/>
+    <github-ribbon href="https://github.com/howdy39" position="left-top" title="#ff0000" color="#ff0000" />
+    <github-ribbon href="https://github.com/howdy39" position="left-bottom" title="#00ff00" color="#00ff00" />
+    <github-ribbon href="https://github.com/howdy39" position="right-bottom" title="#0000ff" color="#0000ff" />
+  </div>
+  `
+  }));
+
+
 /* eslint-enable react/react-in-jsx-scope */
